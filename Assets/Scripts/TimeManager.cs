3f0614696cfedecbf5cad56df84f824eaddb9eb7
ImranBepari97 <imranbepari97@gmail.com>
2020-07-11 00:23:46 +0100

    added basic time

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+
+    public static float time;
+    public float maxTime;
+    public string currentTimeString;
+    void Awake() {
+        time = 0f;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,30 @@ public class TimeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        time += Time.deltaTime;
+        currentTimeString = ReturnTimeReadable();
         
+    }
+
+
+    string ReturnTimeReadable() {
+        int sec = (int) (time % 60);
+        string secFormatted = "";
+        if(sec < 10) {
+            secFormatted = "" + 0 + sec;
+        } else {
+            secFormatted = "" + sec;
+        }
+        
+        int min = (int)  (time % 3600) / 60;
+        string minFormatted = "";
+        if(min < 10) {
+            minFormatted = "" + 0 + min;
+        } else {
+            secFormatted = "" + sec;
+        }
+
+        return "" + minFormatted + ":" + secFormatted;
     }
 }
