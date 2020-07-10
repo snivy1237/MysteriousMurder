63869a86ddee34192961300a0fb4db99b9f294cc
ImranBepari97 <imranbepari97@gmail.com>
2020-07-10 23:28:42 +0100

    added basic player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rigidBody;
+    public int speed;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rigidBody.velocity = new Vector2(
+            Input.GetAxis("Horizontal") * speed,
+            Input.GetAxis("Vertical") * speed
+        );
+    }
+}
