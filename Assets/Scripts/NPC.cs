ecceb308ce7d75844cbbeba8c665eedacf23f5e5
ImranBepari97 <imranbepari97@gmail.com>
2020-07-11 15:32:40 +0100

    removed traces of xNode

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using XNode;
 
-public class DialogueSceneGraph : SceneGraph<DialogueGraph>
+public class NPC : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
