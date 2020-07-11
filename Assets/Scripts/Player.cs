9845c0dfbc1e90649890123fb1c9d7a3f184ea21
nickdaffern27 <45153800+nickdaffern27@users.noreply.github.com>
2020-07-11 10:55:46 +0100

    StartingItemsInteraction
    msh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D rigidBody;
     public int speed;
+    public bool whiteKey;
 
     void Start()
     {
@@ -21,5 +22,17 @@ public class Player : MonoBehaviour
             Input.GetAxis("Horizontal") * speed,
             Input.GetAxis("Vertical") * speed
         );
+
+        //transform.rotation.SetLookRotation(rigidBody.velocity);
+        //transform.rotation.SetLookRotation(rigidBody.velocity);
+
+        //Interaction Call
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Press E");
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 1);
+            Debug.DrawLine(transform.position, transform.position+transform.right*1, Color.green,0.1f);
+            Debug.Log(hit.collider.name);
+        }
     }
 }
