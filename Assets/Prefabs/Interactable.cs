77988d0b4ab489f8fbedd63a96ba6b82001089f6
nickdaffern27 <45153800+nickdaffern27@users.noreply.github.com>
2020-07-11 15:06:35 +0100

    Added Door, Added Interactable class
    read

--- Assets/Prefabs/Interactable.cs
+++ Assets/Prefabs/Interactable.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    public void OnInteract()
-    {
-        //Do nothing
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    virtual public void OnInteract(GameObject objPlayer) {
     }
 }
